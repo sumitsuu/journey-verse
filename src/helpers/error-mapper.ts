856a14a503b4e5cf2d288392eeb269fb5af4f1a9
Stanyslav Parkhomenko <stavezie@gmail.com>
2025-12-25 18:56:39 +0200

    fix: removed axios

--- src/helpers/error-mapper.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosError } from "axios";
-
-export default (error: AxiosError) => {
-  switch (error.message) {
-    case "Request failed with status code 404":
-      return "Requested address not found. Error code: 404. Please contact support.";
-  }
-};
